1af46194b29dfdf098770189a3cfbbe12980e29e
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-03-11 15:28:57 +0900

    [add] script files for install linux-6.6.40-armv7-fpga-1

---
+++ scripts/install-linux-6.6.40-armv7-fpga.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+REPO_DIR=$(cd $(dirname $0); cd .. ; pwd)
+KERNEL_VERSION=6.6.40
+EXTRA_VERSION=-armv7-fpga
+LOCAL_VERSION=
+BUILD_VERSION=1
+
+. "$SCRIPT_DIR/install-variables-armv7-fpga.sh"
+. "$SCRIPT_DIR/install-command.sh"
